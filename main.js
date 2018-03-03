477ad438952549bf22fc95a1a5d7e20e009e2bb1
Melissa Thompson <mthompson828@gmail.com>
2018-03-02 22:38:21 -0500

    clean up js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-window.onload = function() {
+ window.onload = function() {
+
 	function getQuote() {
 		fetch('http://ron-swanson-quotes.herokuapp.com/v2/quotes')
 		  	.then(function(response) {
@@ -7,36 +8,35 @@ window.onload = function() {
 			.then(function(myJson) {
 		    	console.log(myJson);
 		    	showQuote(myJson);
-			});
+		    	return myJson;
+		    })
+		    .then(function(myJson){
+		    	var tweet = document.querySelector('.twitter');
+				tweet.addEventListener("click", function(quote){
+					var tweetURL = 'https://twitter.com/intent/tweet?text="' + myJson + '"';
+				  	window.open(tweetURL,'_blank');
+				}, false);
+				return myJson;
+		    })
+		    .then(function(myJson){
+				var facebook = document.querySelector('.facebook');
+				facebook.addEventListener("click", function(){
+					FB.ui({
+			  		method: 'share',
+			  		href: 'https://developers.facebook.com/docs/',
+			  		quote: quoteDiv.innerHTML,
+					}, function(response){});
+				}, false);
+		    })
 	};
 
-
+	getQuote();
+	
 	var myQuote = document.querySelector('.getquote');
 	myQuote.addEventListener("click", function() {getQuote()});
 
 	var quoteDiv = document.getElementById('quote');
-		function showQuote(quote) {
-			quoteDiv.innerHTML = '"' + quote + '"';
-		}
-
-	var tweet = document.querySelector('.twitter');
-	tweet.addEventListener("click", function(quote){
-		var tweetURL = 'https://twitter.com/intent/tweet?text=' + quoteDiv.innerHTML;
-	  	window.open(tweetURL,'_blank');
-	}, false);
-
-	// var facebook = document.querySelector('.facebook');
-	// facebook.addEventListener("click", function(quote){
-	// 	var facebookURL = 'https://www.facebook.com/sharer/sharer.php?u=example.org';
-	//   	window.open(facebookURL,'_blank');
-	// }, false);
-
-	var facebook = document.querySelector('.facebook');
-	facebook.addEventListener("click", function(){
-		FB.ui({
-  		method: 'share',
-  		href: 'https://developers.facebook.com/docs/',
-  		quote: quoteDiv.innerHTML,
-		}, function(response){});
-	}, false);
+	function showQuote(quote) {
+		quoteDiv.innerHTML = '"' + quote + '"';
+	}
 };
